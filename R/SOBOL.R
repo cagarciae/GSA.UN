d6f6d45577a3538707b1e7e68d548facd494bdf7
cagarciae <59661651+cagarciae@users.noreply.github.com>
2020-04-13 19:25:51 -0500

    First commit

---
+++ R/SOBOL.R
@@ -0,0 +1,41 @@
+#' @name 
+#' SOBOL
+#' 
+#' @title
+#' SOBOL index
+#' 
+#' @description 
+#' This function XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#' 
+#' 
+#' 
+#' 
+#' @param #ENTRY 1 
+#' 
+#' 
+#' @param #ENTRY 2  HASTA EL NUMERO DE ENTRADAS
+#' 
+#' 
+#'
+#' @return 
+#' QUÉ DEVUELVE?
+#' 
+#' @export
+#' 
+#' @author 
+#' Camila Garcia-Echeverri <cagarciae@unal.edu.co> \cr
+#' Maria Cristina Areas-Bautista <mcarenasb@unal.edu.co> \cr
+#' Leonardo David Donado <lddonadog@unal.edu.co> \cr
+#' 
+#' 
+#' Hydrodynamics of the natural media research group - HYDS
+#' Universidad Nacional de Colombia - sede Bogota
+#' 
+#' @examples
+#' 
+#' 
+#' 
+
+SOBOL <- function(x,y){
+  x <- 1+1
+}
