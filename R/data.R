0ac9d00073b3fe80a26f8e0c5312108f4cfa42bf
cagarciae <cagarciae@unal.edu.co>
2020-04-15 18:03:20 -0500

    First commit of exampleFun dataset

---
+++ R/data.R
@@ -0,0 +1,131 @@
+#'  @title  Example function results
+#'
+#'  @description Data generated with an example funtion
+#'
+#'
+#'@format A \code{matrix}
+#' \describe{
+#'    \item{out_set}{a vector with alpha1 values for each GRU}
+#' }
+#'
+#' @source Synthetic generation
+#'
+#' @author CGE
+#'
+#' @examples
+#' \dontrun{
+#' data(ExampleFUN)
+#' summary(ExampleFUN)
+#' }
+#'
+"out_set"
+
+#'  @title  Example function results
+#'
+#'  @description Data generated with an example funtion
+#'
+#'
+#'@format A \code{matrix}
+#' \describe{
+#'    \item{parameters_set}{a vector with alpha1 values for each GRU}
+#' }
+#'
+#' @source Synthetic generation
+#'
+#' @author CGE
+#'
+#' @examples
+#' \dontrun{
+#' data(ExampleFUN)
+#' summary(ExampleFUN)
+#' }
+#'
+"parameters_set"
+
+#'  @title  Example function results
+#'
+#'  @description Data generated with an example funtion
+#'
+#'
+#'@format A \code{vector}
+#' \describe{
+#'    \item{z}{a vector with alpha1 values for each GRU}
+#' }
+#'
+#' @source Synthetic generation
+#'
+#' @author CGE
+#'
+#' @examples
+#' \dontrun{
+#' data(ExampleFUN)
+#' summary(ExampleFUN)
+#' }
+#'
+"z"
+
+#'  @title  Example function results
+#'
+#'  @description Data generated with an example funtion
+#'
+#'
+#'@format A \code{vector}
+#' \describe{
+#'    \item{a}{a vector with alpha1 values for each GRU}
+#' }
+#'
+#' @source Synthetic generation
+#'
+#' @author CGE
+#'
+#' @examples
+#' \dontrun{
+#' data(ExampleFUN)
+#' summary(ExampleFUN)
+#' }
+#'
+"a"
+
+#'  @title  Example function results
+#'
+#'  @description Data generated with an example funtion
+#'
+#'
+#'@format A \code{vector}
+#' \describe{
+#'    \item{b}{a vector with alpha1 values for each GRU}
+#' }
+#'
+#' @source Synthetic generation
+#'
+#' @author CGE
+#'
+#' @examples
+#' \dontrun{
+#' data(ExampleFUN)
+#' summary(ExampleFUN)
+#' }
+#'
+"b"
+
+#'  @title  Example function results
+#'
+#'  @description Data generated with an example funtion
+#'
+#'
+#'@format A \code{vector}
+#' \describe{
+#'    \item{c}{a vector with alpha1 values for each GRU}
+#' }
+#'
+#' @source Synthetic generation
+#'
+#' @author CGE
+#'
+#' @examples
+#' \dontrun{
+#' data(ExampleFUN)
+#' summary(ExampleFUN)
+#' }
+#'
+"c"
