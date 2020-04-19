24a29d6de021ffdd0301b66eadb60c08cec203f5
cagarciae <cagarciae@unal.edu.co>
2020-04-18 22:15:47 -0500

    Cond_Moments function was created

--- R/Cond_Moments.R
+++ R/Cond_Moments.R
@@ -5,20 +5,25 @@
 #' Conditional statistical moments
 #'
 #' @description
-#' This function XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#' This function evaluates the first four statistical moments
+#' after grouping the model output by differents parametric
+#' ranges
 #'
 #'
+#' @param parameters_set matrix of dimensions n x pp, where n is the
+#' number of runs and pp is the number of parameters.
 #'
+#' @param out_set matrix of dimensions n x t, where n is the number of
+#' runs and t is the number of tempory steps.
 #'
-#' @param #ENTRY 1
+#' @param pp_names vector that contains the names of the parameters
 #'
-#'
-#' @param #ENTRY 2  HASTA EL NUMERO DE ENTRADAS
-#'
+#' @param steps number of divisions of the parametric range
 #'
 #'
 #' @return
-#' QUÉ DEVUELVE?
+#' A list of arrays, each array has dimensions of steps, t, p.
+#'
 #'
 #' @export
 #'
@@ -31,13 +36,14 @@
 #' Universidad Nacional de Colombia - sede Bogota
 #'
 #' @examples
-#'
-#'
+#' data("parameters_set", "out_set", "pp_names")
+#' CM <- Cond_Moments(parameters_set, out_set, pp_names)
 #'
 
-Cond_Moments <- function(parameters_set, out_set , pp_names, steps = 10, t){ #pensar en t
+Cond_Moments <- function(parameters_set, out_set , pp_names, steps = 10){
 
   pp <- dim(parameters_set)[2]
+  t <- dim(out_set)[2]
 
   CM_count<-array(dim=c(steps,t,pp),dimnames = list(NULL,NULL,pp_names))
   CM_mean<-array(dim=c(steps,t,pp),dimnames = list(NULL,NULL,pp_names))
@@ -92,11 +98,11 @@ Cond_Moments <- function(parameters_set, out_set , pp_names, steps = 10, t){ #pe
 
         mean__[i]<-mean(out_set[a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s]),f], na.rm = T)
 
-        var__[i]<-stats::var(out_set[a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s]),f])
+        var__[i]<-stats::var(out_set[a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s]),f], na.rm = T)
 
-        skw__[i]<-e1071::skewness(out_set[a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s]),f])
+        skw__[i]<-e1071::skewness(out_set[a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s]),f], na.rm = T)
 
-        kurt__[i]<-(e1071::kurtosis(out_set[a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s]),f]))+3
+        kurt__[i]<-(e1071::kurtosis(out_set[a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s]),f], na.rm = T))+3
 
       }
       CM_count[,f,s]<-count__
@@ -106,7 +112,10 @@ Cond_Moments <- function(parameters_set, out_set , pp_names, steps = 10, t){ #pe
       CM_kurt[,f,s]<-kurt__
     }
   }
+  CM <- list("CM_count"=CM_count, "CM_mean"=CM_mean, "CM_var"=CM_var, "CM_skw"=CM_skw, "CM_kurt"=CM_kurt)
 
-  return(CM_count,CM_mean,CM_skw,CM_var,CM_kurt)
+
+  return(CM)
 }
+
 
