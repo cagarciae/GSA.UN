77c0f9a815848eb2b69efa71382ab894007937d3
cagarciae <cagarciae@unal.edu.co>
2020-05-04 19:23:46 -0500

    some warnings were added

--- R/Cond_Moments.R
+++ R/Cond_Moments.R
@@ -41,80 +41,89 @@
 
 Cond_Moments <- function(parameters_set, out_set , pp_names, steps = 100){
 
-  pp <- dim(parameters_set)[2]
-  t <- dim(out_set)[2]
+  if(!is.matrix(parameters_set) | !is.matrix(out_set)){
+    warning("parameters_set and out_set must be matrices")
+  }else if (!is.vector(pp_names)) {
+    warning("pp_names must be a character vector")
+  }else if (dim(parameters_set)[1] != dim(out_set)[1]){
+    warning("There is a mismatch in the number of runs between parameters_set and out_set")
+  }else{
 
-  CM_count<-array(dim=c(steps,t,pp),dimnames = list(NULL,NULL,pp_names))
-  CM_mean<-array(dim=c(steps,t,pp),dimnames = list(NULL,NULL,pp_names))
-  CM_var<-array(dim=c(steps,t,pp),dimnames = list(NULL,NULL,pp_names))
-  CM_skw<-array(dim=c(steps,t,pp),dimnames = list(NULL,NULL,pp_names))
-  CM_kurt<-array(dim=c(steps,t,pp),dimnames = list(NULL,NULL,pp_names))
+    pp <- dim(parameters_set)[2]
+    t <- dim(out_set)[2]
 
-
-  div <- dim(parameters_set)[1]/steps
-  sorted <- apply(parameters_set,2,sort)
-
-  ranges_pp <- matrix(nrow=3*steps,ncol=pp)
-  colnames(ranges_pp) <- pp_names
-
-  # cond_pp <- matrix(nrow=steps,ncol=pp) revisar
-  # colnames(cond_pp) <- pp_names
-
-  for (s in 1:pp){
-    x <- 0
-    a <- 1
-    for(i in 1:steps){
-      x <- (3*i)
-      ranges_pp[(x-2),s] <- min(sorted[a:(div*i),s])
-      ranges_pp[(x-1),s] <- max(sorted[a:(div*i),s])
-      ranges_pp[(x),s] <- ranges_pp[(x-1),s]-ranges_pp[(x-2),s]
-      #cond_pp[i,s] <- ranges_pp[(x-1),s]-ranges_pp[(x-2),s] revisar
-
-      a <- a+div
-    }
-  }
-
-  ranges_pp<-as.data.frame(ranges_pp)
-  #cond_pp<-as.data.frame(cond_pp) revisar
-
-  #a <- cond_pp[1,]-cond_pp[2,]; aa <- cond_pp[(steps),]-cond_pp[(steps-1),] #revisar
+    CM_count<-array(dim=c(steps,t,pp),dimnames = list(NULL,NULL,pp_names))
+    CM_mean<-array(dim=c(steps,t,pp),dimnames = list(NULL,NULL,pp_names))
+    CM_var<-array(dim=c(steps,t,pp),dimnames = list(NULL,NULL,pp_names))
+    CM_skw<-array(dim=c(steps,t,pp),dimnames = list(NULL,NULL,pp_names))
+    CM_kurt<-array(dim=c(steps,t,pp),dimnames = list(NULL,NULL,pp_names))
 
 
-  for(f in 1:t){
+    div <- dim(parameters_set)[1]/steps
+    sorted <- apply(parameters_set,2,sort)
 
-    #calculo de momentos para cada intervalo
+    ranges_pp <- matrix(nrow=3*steps,ncol=pp)
+    colnames(ranges_pp) <- pp_names
+
+    # cond_pp <- matrix(nrow=steps,ncol=pp) revisar
+    # colnames(cond_pp) <- pp_names
 
     for (s in 1:pp){
-
-      count__<-vector("numeric",0); mean__<-vector("numeric",0); var__<-vector("numeric",0);
-      skw__<-vector("numeric",0); kurt__<-vector("numeric",0)
-
+      x <- 0
+      a <- 1
       for(i in 1:steps){
+        x <- (3*i)
+        ranges_pp[(x-2),s] <- min(sorted[a:(div*i),s])
+        ranges_pp[(x-1),s] <- max(sorted[a:(div*i),s])
+        ranges_pp[(x),s] <- ranges_pp[(x-1),s]-ranges_pp[(x-2),s]
+        #cond_pp[i,s] <- ranges_pp[(x-1),s]-ranges_pp[(x-2),s] revisar
 
-        a<-parameters_set[,s]
-
-        count__[i]<-(sum(a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s])))/length(a)
-
-        mean__[i]<-mean(out_set[a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s]),f], na.rm = T)
-
-        var__[i]<-stats::var(out_set[a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s]),f], na.rm = T)
-
-        skw__[i]<-e1071::skewness(out_set[a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s]),f], na.rm = T)
-
-        kurt__[i]<-(e1071::kurtosis(out_set[a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s]),f], na.rm = T))+3
-
+        a <- a+div
       }
-      CM_count[,f,s]<-count__
-      CM_mean[,f,s]<-mean__
-      CM_var[,f,s]<-var__
-      CM_skw[,f,s]<-skw__
-      CM_kurt[,f,s]<-kurt__
     }
+
+    ranges_pp<-as.data.frame(ranges_pp)
+    #cond_pp<-as.data.frame(cond_pp) revisar
+
+    #a <- cond_pp[1,]-cond_pp[2,]; aa <- cond_pp[(steps),]-cond_pp[(steps-1),] #revisar
+
+
+    for(f in 1:t){
+
+      #calculo de momentos para cada intervalo
+
+      for (s in 1:pp){
+
+        count__<-vector("numeric",0); mean__<-vector("numeric",0); var__<-vector("numeric",0);
+        skw__<-vector("numeric",0); kurt__<-vector("numeric",0)
+
+        for(i in 1:steps){
+
+          a<-parameters_set[,s]
+
+          count__[i]<-(sum(a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s])))/length(a)
+
+          mean__[i]<-mean(out_set[a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s]),f], na.rm = T)
+
+          var__[i]<-stats::var(out_set[a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s]),f], na.rm = T)
+
+          skw__[i]<-e1071::skewness(out_set[a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s]),f], na.rm = T)
+
+          kurt__[i]<-(e1071::kurtosis(out_set[a>(ranges_pp[((3*i)-2),s])&a<=(ranges_pp[((3*i)-1),s]),f], na.rm = T))+3
+
+        }
+        CM_count[,f,s]<-count__
+        CM_mean[,f,s]<-mean__
+        CM_var[,f,s]<-var__
+        CM_skw[,f,s]<-skw__
+        CM_kurt[,f,s]<-kurt__
+      }
+    }
+    CM <- list("CM_count"=CM_count, "CM_mean"=CM_mean, "CM_var"=CM_var, "CM_skw"=CM_skw, "CM_kurt"=CM_kurt)
+
+
+    return(CM)
   }
-  CM <- list("CM_count"=CM_count, "CM_mean"=CM_mean, "CM_var"=CM_var, "CM_skw"=CM_skw, "CM_kurt"=CM_kurt)
-
-
-  return(CM)
 }
 
 
