c37805f97120ef188e3a708a80e65936891695dd
cagarciae <cagarciae@unal.edu.co>
2020-06-16 17:10:05 -0500

    style check

--- R/Bstat.R
+++ R/Bstat.R
@@ -39,29 +39,29 @@ Bstat <- function(out_set){
   }else{
 
 
-  sufix <- c(Mean="mean",Variance="var",Skewness="skw",Kurtosis="kurt", Kurtosis_ex="kurt_ex")
+    sufix <- c(Mean="mean",Variance="var",Skewness="skw",Kurtosis="kurt", Kurtosis_ex="kurt_ex")
 
-  for(i in 1:5){
-    a<-paste(sufix[i],"_t",sep="")
-    aa<-vector("numeric",0)
-    assign(a,aa)
-  }
+    for(i in 1:5){
+      a <- paste(sufix[i],"_t",sep="")
+      aa <- vector("numeric",0)
+      assign(a,aa)
+    }
 
-  t <- dim(out_set)[2]
+    t <- dim(out_set)[2]
 
-  #calculates the first four statistical moments
-  for(f in 1:t){
-    mean_t[f]<-mean(out_set[,f])
-    var_t[f]<-stats::var(out_set[,f])
-    skw_t[f]<- e1071::skewness(out_set[,f])
-    kurt_t[f]<- mean((out_set[,f]-mean_t[f])^4)/mean((out_set[,f]-mean_t[f])^2)^2
-    kurt_ex_t[f]<- e1071::kurtosis(out_set[,f])
-  }
+    #calculates the first four statistical moments
+    for(f in 1:t){
+      mean_t[f] <- mean(out_set[,f])
+      var_t[f] <- stats::var(out_set[,f])
+      skw_t[f] <- e1071::skewness(out_set[,f])
+      kurt_t[f] <- mean((out_set[,f]-mean_t[f])^4)/mean((out_set[,f]-mean_t[f])^2)^2
+      kurt_ex_t[f] <- e1071::kurtosis(out_set[,f])
+    }
 
-  data_Bstat<- data.frame(t<-c(1:t), Media_t<-mean_t,
-                             Var_t<-var_t, Skw_t<-skw_t, Kurt_t<-kurt_t,
-                             Kurt_ex_t<-kurt_ex_t)
+    data_Bstat <- data.frame(t<-c(1:t), Media_t<-mean_t,
+                            Var_t<-var_t, Skw_t<-skw_t, Kurt_t<-kurt_t,
+                            Kurt_ex_t<-kurt_ex_t)
 
-  return(data_Bstat)
+    return(data_Bstat)
   }
 }
